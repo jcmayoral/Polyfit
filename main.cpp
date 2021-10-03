a7f5f4303fa0c057b180fe453676178387b225f1
jcmayoral <jocamaba1989@gmail.com>
2021-10-03 17:14:19 +0200

    atoi

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(int argc, const char * argv[]) {
     for (int i = 0; i< argc ; i++)
 	 std::cout << "ARG " << argv[i] << std::endl;
     
-    int degree = int(*argv[1]);
+    int degree = atoi(argv[1]);
     std::cout << "Polynomial degree " << degree << std::endl;
     
     for (int i = 0; i <= 5; ++i)
